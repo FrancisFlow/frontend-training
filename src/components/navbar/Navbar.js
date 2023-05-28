d3fe49268811f0927e12a4463651568f23c99d42
francisflow <Iamngigikaranja1@aol.com>
2023-05-28 14:33:00 +0300

    Updated main with a class of page-wrapper

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,28 @@
 const Navbar = () => {
     return (  
         <>
-        
+      <header className="header js-header">
+        <div className="container">
+          <div className='logo'>
+            <a href="/">Tolmol <span>cleaning services</span></a>
+          </div>
+          <button type="button" class="nav-toggler js-nav-toggler">
+            <span></span>
+          </button>
+          <nav className="nav js-nav">
+            <ul>
+              <li><a href="/home">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/pricing">Pricing</a></li>
+              <li><a href="/team">Team</a></li>
+              <li><a href="/contact"></a>Contact</li>
+            </ul>
+          </nav>
+        </div>
+
+      </header>
+     
         </>
     );
 }
