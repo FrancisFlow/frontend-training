fc9a2876f5777bbe7cc0837d8b8d5a1c1d7bf9b8
francisflow <Iamngigikaranja1@aol.com>
2023-05-30 11:26:27 +0300

    Modification of fonts with tailwindcss

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
           </button>
           <nav className="nav js-nav">
             <ul>
-              <li><a href="/home">Home</a></li>
+              <li><a href="#home">Home</a></li>
               <li><a href="/about">About</a></li>
               <li><a href="/services">Services</a></li>
               <li><a href="/pricing">Pricing</a></li>
@@ -41,6 +41,7 @@ const Navbar = () => {
         </div>
 
       </header>
+      
      
         </>
     );
