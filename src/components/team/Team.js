1b7d839d5bf468a2464feef8e6f90ddaf2d0024a
francisflow <Iamngigikaranja1@aol.com>
2023-06-02 10:08:57 +0300

    Created a component for the team section

---
+++ src/components/team/Team.js
@@ -0,0 +1,18 @@
+const Team = () => {
+    return ( 
+        <>
+    <section className="team section-padding">
+        <div className="container">
+            <div className="team-text">
+                <span>team</span>
+                <h3 className="title"></h3>
+            </div>
+            <div className="team-area">
+                <p>I did some  coding today</p>
+            </div>
+        </div>
+        </section></>
+     );
+}
+ 
+export default Team;
