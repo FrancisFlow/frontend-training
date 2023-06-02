441b3b793031cf42ab41a37ccac6166d41fca98b
francisflow <Iamngigikaranja1@aol.com>
2023-06-02 12:52:51 +0300

    Added section id links to navigation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -31,11 +31,11 @@ const Navbar = () => {
           <nav className="nav js-nav">
             <ul>
               <li><a href="#home">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/pricing">Pricing</a></li>
-              <li><a href="/team">Team</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#pricing">Pricing</a></li>
+              <li><a href="#team">Team</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           </nav>
         </div>
